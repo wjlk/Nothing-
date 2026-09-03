29c88f54f6d8ef94d470b3ef4d16516190aaef7c
wjlk <82113671+wjlk@users.noreply.github.com>
2026-09-03 18:46:17 +0300

    Add Android app file: sender/src/main/java/com/example/localalert/sender/BootReceiver.java

---
+++ sender/src/main/java/com/example/localalert/sender/BootReceiver.java
@@ -0,0 +1,18 @@
+package com.example.localalert.sender;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+
+public class BootReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Intent serviceIntent = new Intent(context, SenderService.class);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            context.startForegroundService(serviceIntent);
+        } else {
+            context.startService(serviceIntent);
+        }
+    }
+}
